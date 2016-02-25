89ee8957d045d4f1ffe8f154c0614941fdebeca7
jim005 <james@websenso.com>
2016-02-25 14:09:58 +0000

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # link2nativemaps
-Script PHP to redirect to native map apps
+Script PHP to redirect to native map apps.
+
+In our website, email, sms... we could forward users to url like this 
+http://maps.appsenso.eu/?q=44.533744,6.471383
+so on mobile devices they are redirected to their native maps apps. Otherwise, they're redirecting to Google Maps.
+
+# Live demo & webservice
+This script is used here http://maps.appsenso.eu
+Feel free to use it. Exemeple : 
+http://maps.appsenso.eu/?q=44.533744,6.471383
+http://maps.appsenso.eu/?q=place+saint+marcellin,+05000+Gap,+France
+
+
+# Credits & Thanks
+https://github.com/Sjors/currentLocationStringForCurrentLanguage
+https://www.habaneroconsulting.com/insights/opening-native-map-apps-from-the-mobile-browser
+http://jsfiddle.net/johnallan/YkMA2/
+http://www.metamodpro.com/browser-language-codes
+
+
---
+++ index.php
@@ -0,0 +1,79 @@
+<?php 
+/**
+/* by: jim007 (websenso.com)
+/* ver: 0.1
+/* date: Feb 2016
+*/
+
+/** 
+ * Implementation looks like : 
+ * http://maps.appsenso.eu/?q=44.533744,6.471383
+ * 
+*/
+
+
+// Get data
+if (!isset($_GET['q'])) {
+    header('Location: https://www.websenso.com/');
+    exit;
+} else {
+    $destination = urlencode($_GET['q']);
+}
+
+
+// Redirect
+$UserAgent = $_SERVER['HTTP_USER_AGENT'];
+if (strpos($UserAgent,"Windows Phone")){
+    $url = "maps:" . $destination;
+}
+elseif (strpos($UserAgent,"Android")) {
+    $url = "geo:" . $destination;
+}
+elseif (strpos($UserAgent,"Windows Phone")){
+    $url = "maps:" . $destination;
+}
+elseif (strpos($UserAgent,"AppleWebKit")) {
+    $url = "maps:?saddr=" .  urlencode(GetCurrentLocationTranslated()) . "&daddr=" . $destination;
+}
+else {
+    $url = "http://maps.google.com?q=" . $destination;
+}
+
+header('Location: ' . $url);
+
+
+
+/**
+ * Return label Current Location in the user's language. Useful for iOs
+ * 
+ */
+function GetCurrentLocationTranslated ()  {
+    
+    $browser_lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    switch($browser_lang) {
+        
+        case "fr":
+        case "fr-fr":
+        case "fr-be":
+        case "fr-ca":
+        case "fr-lu":
+            $CurrentLocationTranslated  = "Lieu actuel";
+        break;
+
+        case "it":
+        case "it-ch":
+            $CurrentLocationTranslated  = "Posizione attuale";
+        break;
+        
+        case "nl":
+        case "nl-be":
+            $CurrentLocationTranslated  = "Huidige locatie";
+        break;
+
+        default:
+            $CurrentLocationTranslated  = "Current Location";
+        break;
+    }
+    
+    return $CurrentLocationTranslated;
+} 
