7624e3290c5afd427def984ad1fa7ad0d406000b
Jim005 <james@websenso.com>
2022-06-25 16:58:56 +0200

    add LL parameter

---
+++ error-routing.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Error routing</title>
+      <meta http-equiv = "refresh" content = "2; url = https://github.com/jim005/link2nativemaps/" />
+   </head>
+   <body>
+      <p>Error routing: read the documentation please.</p>
+   </body>
+</html>
--- index.php
+++ index.php
@@ -5,41 +5,58 @@
  * date: Aout 2019
  *
  * 
- * Implementation looks like : 
- * http://maps.appsenso.eu/?q=44.533744,6.471383
+ * Implementation like : 
+ * https://maps.appsenso.eu/?q=44.533744,6.471383
  * 
 */
 
 
 // Get data
-if (!isset($_GET['q'])) {
-    	header('Location: https://www.openstreetmap.org');
+if (!isset($_GET['q']) || empty($_GET['q']) ) {
+    	header('Location: /error-routing.php');
     	exit;
 } else {
-    	$destination = urlencode($_GET['q']);
-	$latlon_array = explode("%2C", $destination);
-	$lat = (float) $latlon_array[0];
-	$lon = (float) $latlon_array[1]; 
+
+	$q = $_GET['q'];
+
+	// Find Lat and Lon, for OpenStreetMap URL.
+	$re = '/(^\-?\d+\.*\d*\,\-?\d+\.*\d*)/m'; // Check if lat/long value, like :  44.545452,6.276119
+	preg_match_all($re, $q, $matches, PREG_SET_ORDER, 0);
+	$lat = null;
+	$lon = null;
+	if ($matches)  {
+       	$latlon_array = explode(",", $matches[0][0]);
+		$lat = (float) $latlon_array[0];
+		$lon = (float) $latlon_array[1];
+	}
+
+	// query string
+    $q = urlencode($q);
+
+	// The location around which the map should be centered. A comma-separated pair of floating point values that represent latitude and longitude (in that order).
+	$ll = isset($_GET['ll']) ? urlencode($_GET['ll']) : null;
+
 }
+
+
 
 // Redirect
 $UserAgent = $_SERVER['HTTP_USER_AGENT'];
 if (strpos($UserAgent,"Windows Phone")){
-   	 $url = "maps:" . $destination;
+	$url = "maps:" . $q;
 }
 elseif (strpos($UserAgent,"Android")) {
-    	$url = "geo:" . $destination;
-}
-elseif (strpos($UserAgent,"Windows Phone")){
-    	$url = "maps:" . $destination;
+	$url = "geo:" . $q;
+	$url .= ($ll) ? "&ll=$ll" : null;
+
 }
 elseif (strpos($UserAgent,"AppleWebKit")) {
-	$url = "https://maps.apple.com/?q=$lat,$lon&sll=$lat,$lon";
+	$url = "https://maps.apple.com/?q=$q";
+	$url .= ($ll) ? "&ll=$ll" : null;
 }
 else {
-	$url = "https://www.openstreetmap.org/?mlat=$lat&mlon=$lon#map=17/$lat/$lon"; 
+	$url = ($lat && $lon) ? "https://www.openstreetmap.org/?mlat=$lat&mlon=$lon#map=17/$lat/$lon" : "https://www.openstreetmap.org/";
 }
 
-header('Location: ' . $url);
-
+header('Location: ' . $url, TRUE, 307);
 
